9ca07783903b168b700181946e64787283fdf298
Michael Jalkio <mjalkio@gmail.com>
2015-04-19 03:27:43 -0400

    Started to create Twilio SMS interface

--- run.py
+++ run.py
@@ -1,10 +1,71 @@
-from flask import Flask
+from settings import FLASK_SECRET_KEY, KNOWN_NUMBERS
+import spotify
+
+from flask import Flask, request, session
+import twilio.twiml
+
 app = Flask(__name__)
+app.config.from_object(__name__)
+app.secret_key = FLASK_SECRET_KEY
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/', methods=['GET', 'POST'])
+def hello_monkey():
+    """Respond with the number of text messages sent between two parties."""
+
+    counter = session.get('counter', 0)
+
+    # increment the counter
+    counter += 1
+
+    # Save the new counter value in the session
+    session['counter'] = counter
+
+    from_number = request.values.get('From')
+    if from_number in KNOWN_NUMBERS:
+        name = KNOWN_NUMBERS[from_number]
+    else:
+        name = "Monkey"
+
+    message = "".join([name, " has messaged ", request.values.get('To'), " ",
+                      str(counter), " times."])
+    resp = twilio.twiml.Response()
+    resp.sms(message)
+
+    return str(resp)
+
+
+@app.route('/twilio', methods=['POST'])
+def process_twilio():
+    text = request.values.get('Body').lower().strip()
+    if text == 'playlists':
+        playlists = spotify.get_wejam_playlists()
+        message = ','.join(playlists)
+        message = 'Playlists: ' + message
+    elif text.startswith('tracks'):
+        playlist_name = text[7:]
+        try:
+            tracks = spotify.track_listing(playlist_name)
+            message = ''
+            for i, song in enumerate(tracks):
+                message += '{0}) {1}\n'.format(i + 1, song.title)
+            if len(message) >= 1600:
+                message = message[:1595] + '...'
+        except spotify.SpotifyException, e:
+            message = str(e)
+    else:
+        message = "Unsupported command.  Valid commands are: "
+        for command in ['playlists', 'tracks [playlist name]']:
+            message += '"{0}"'.format(command)
+    resp = twilio.twiml.Response()
+    resp.sms(message)
+    return str(resp)
+
+
+@app.route('/callback', methods=['GET'])
+def spotify_callback():
+    return 'Authenticated successfully with Spotify!'
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- spotify.py
+++ spotify.py
@@ -1,11 +1,12 @@
 from settings import (SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET,
                       SPOTIFY_REDIRECT_URI, SPOTIFY_USER_ID)
 import spotipy
+from spotipy.client import SpotifyException
 import spotipy.util as util
 
 # A dict of playlists currently in We Jam
 # key = playlist name, value = playlist id
-wejam_playlists = {'Best of Disney': '2dKLJaJTw0YC8U5Rd9GXKy'}
+wejam_playlists = {'best of disney': '2dKLJaJTw0YC8U5Rd9GXKy'}
 
 
 class Song:
@@ -25,7 +26,8 @@ def spotify():
     if token:
         return spotipy.Spotify(auth=token)
     else:
-        print("Couldn't get token for user {0}".format(SPOTIFY_USER_ID))
+        raise spotipy.oauth2.SpotifyOauthError("Couldn't get token for user \
+                                                {0}".format(SPOTIFY_USER_ID))
 
 
 def get_wejam_playlists():
@@ -33,12 +35,23 @@ def get_wejam_playlists():
 
 
 def create_playlist(playlist_name):
-    return []
+    sp = spotify()
+
+    response = sp.user_playlist_create(SPOTIFY_USER_ID, playlist_name)
+
+    if 'id' in response:
+        playlist_id = response['id']
+        wejam_playlists[playlist_name] = playlist_id
+    else:
+        raise SpotifyException(response['error']['status'],
+                               'Could not create playlist.')
 
 
 def track_listing(playlist_name):
     if playlist_name not in wejam_playlists:
-        return '{0} is not currently managed by We Jam.'
+        raise SpotifyException(400, -1,
+                               '{0} is not currently managed by We Jam.'.
+                               format(playlist_name))
 
     sp = spotify()
 
@@ -59,3 +72,15 @@ def track_listing(playlist_name):
         track_list.append(Song(track['name'], artists))
 
     return track_list
+
+
+def add_track(playlist_name, track_id):
+    if playlist_name not in wejam_playlists:
+        msg = '{0} is not currently managed by We Jam.'.format(playlist_name)
+        raise spotipy.client.SpotifyException(400, -1, msg)
+
+    sp = spotify()
+
+    sp.user_playlist_add_tracks(SPOTIFY_USER_ID,
+                                wejam_playlists[playlist_name],
+                                track_id)
