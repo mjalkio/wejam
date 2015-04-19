2e0b0a2bd74928b03d63688ddaa762a84eab7a0a
Michael Jalkio <mjalkio@gmail.com>
2015-04-19 00:42:34 -0400

    Remove send_sms.py

--- send_sms.py
+++
@@ -1,9 +0,0 @@
-from twilio.rest import TwilioRestClient
-
-# Find these values at https://twilio.com/user/account
-account_sid = "ACXXXXXXXXXXXXXXXXX"
-auth_token = "YYYYYYYYYYYYYYYYYY"
-client = TwilioRestClient(account_sid, auth_token)
-
-message = client.messages.create(to="+12316851234", from_="+15555555555",
-                                 body="Hello there!")
